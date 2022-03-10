f68e201571d9a22387d8f2c4fbc5fc4560603f31
Bella <shdudtnr3939@naver.com>
2022-03-10 12:21:37 +0900

    22.03.10 Clock01 error modified

--- 1.basics/ex08/src/06/Clock01.js
+++ 1.basics/ex08/src/06/Clock01.js
@@ -2,15 +2,18 @@ import React from 'react'
 
 const Clock01 = () => {
   const date = new Date();
-  const hours = String(date.getHours()).padStart(2,"0");
-  const minutes = String(date.getMinutes()).padStart(2,"0");
-  const seconds = String(date.getSeconds()).padStart(2,"0");
-  const currentTime = 'AM';
-  if(hours > 12){
-    currentTime = 'PM';
+  let hours = date.getHours();
+  if(hours < 10 ){    // 2자리수로 나오게끔 처리
+    hours = '0' + hours;
   }
+  const minutes = String(date.getMinutes()).padStart(2,"0");
+  const seconds = ('0'+date.getSeconds()).slice(-2);
+
+  const currentTime = (hours > 11) ? 'PM' : 'AM';
+
   return (
     <div>{currentTime} {hours}:{minutes}:{seconds}</div>
+    // <div>{currentTime} {hours}:{minutes}:{('0'+date.getSeconds()).slice(-2)}</div> 가능하면 변수를 위에 두지 않고 표현식 안에서 모든 처리를 하기
   )
 }
 
