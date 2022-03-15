124fed2437a72dcffe4b2364bcbbc9129d3329b9
Bella <shdudtnr3939@naver.com>
2022-03-15 10:20:29 +0900

    22.03.15 fontawesome

--- 2.Component/ex02/src/06/App.js
+++ 2.Component/ex02/src/06/App.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import styles from './assets/scss/App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBell, faCheckCircle, faTimesCircle} from '@fortawesome/free-solid-svg-icons';
+
+// additional...
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {far} from '@fortawesome/free-regular-svg-icons';
+import {fab} from '@fortawesome/free-brands-svg-icons';
 
 const App = () => {
+  library.add(faBell, far, fab);
+
   return (
-    <div className={styles.App}>
-      <h1 className={styles.Header}>{`SASS & SCSS:(css-loader options: {modules: true})`}</h1>
+    <div>
+      { /** example */}
+      <FontAwesomeIcon icon={faBell} />
+      <FontAwesomeIcon icon={faCheckCircle} />
+      <FontAwesomeIcon icon={faTimesCircle} />
+
+      { /* solid */ }
+      <FontAwesomeIcon icon= {["fas", "bell"]} />
+      <FontAwesomeIcon icon= {"bell"} />    
+
+      { /* regular */ }
+      <FontAwesomeIcon icon= {["far", "bell"]} />
+
+      {/* brands */}
+      <FontAwesomeIcon icon= {["fab", "github"]} />
+      <FontAwesomeIcon icon= {["fab", "google"]} />
+      <FontAwesomeIcon icon= {["fab", "apple"]} />
+
     </div>
   )
 }
