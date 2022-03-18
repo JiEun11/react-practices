7e4ba25963d39c7cd9406cc92464df9fa0a6a305
Bella <shdudtnr3939@naver.com>
2022-03-18 11:14:15 +0900

    22.03.18 Kanbanboard hint2

--- 2.Component/ex04/src/02/Form.js
+++ 2.Component/ex04/src/02/Form.js
@@ -6,7 +6,12 @@ import {faCheckCircle, faTimesCircle} from '@fortawesome/free-solid-svg-icons';
 export default function Form() {
     const [name, setName] = useState("Bella");
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [validEmail, setValidEmail] = useState(false);  // UI용, 체크박스 띄울지 X박스 띄울지
+    const [gender, setGender] = useState('female');   // default값도 세팅
+    const [birthYear, setBirthYear] = useState('1984');
+    const [description, setDescription] = useState("");
+    const [agreeProv, setAgreeProv] = useState('no');
 
     const onChangeInputName = e => {
       // setName(e.target.value);
@@ -21,6 +26,34 @@ export default function Form() {
       const re =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       setValidEmail(re.test(e.target.value));
     };
+
+    // const onChangeInputPassword = e => {
+    //   setPassword(e.target.value);
+    // }; 내부에 구현함.
+
+    const onChangeInputGender = function(e) {
+      setGender(e.target.value);  // 누르는 버튼의 value를 상태값에 세팅
+      console.log(e.target.value);
+    }
+
+    const onChangeAgreeProv = function(e){
+      // console.log(e.target.value);
+      // if(e.target.value==='no'){
+      //   setAgreeProv('yes');
+      // }
+      // if(e.target.value==='yes'){
+      //   setAgreeProv('no');
+      // }
+      const status = e.target.value === 'no' ? 'yes' : 'no';
+      
+      // API 호출을 하게 된다.
+      const url = `/prov/agree? status=${status}`;
+      // result = await fetch(url);
+      console.log(url);
+      if(true){
+        setAgreeProv(status);
+      }
+    }
 
     return (
         <form id="joinForm" name="joinForm" method="post" action="/do/not/post">
@@ -49,16 +82,38 @@ export default function Form() {
               }            
 
             <label htmlFor="password">패스워드</label>
-            <input id="password" name="password" type="password" value={ "" } />
+            <input 
+              id="password" 
+              name="password" 
+              type="password" 
+              value={ password } 
+              onChange={e => setPassword(e.target.value)}/>
 
             <fieldset>
                 <legend>성별</legend>
-                <label>여</label> <input type="radio" name="gender" value={ "female" } defaultChecked={ true } />
-                <label>남</label> <input type="radio" name="gender" value={ "male" } defaultChecked={ false } />
+                <label>여</label> 
+                  <input 
+                    type="radio" 
+                    name="gender" 
+                    value={ "female" } 
+                    // defaultChecked={ true } , 제어 컴포넌트로 만들었기 때문에 필요가 없어짐
+                    checked={gender === 'female'} 
+                    onChange={onChangeInputGender}/>
+                <label>남</label> 
+                  <input 
+                    type="radio" 
+                    name="gender" 
+                    value={ "male" } 
+                    // defaultChecked={ false } 
+                    checked={gender === 'male'}
+                    onChange={onChangeInputGender} />
             </fieldset>
 
             <label htmlFor="birthYear">생년</label>
-            <select id="birthYear">
+            <select 
+              id="birthYear" 
+              value={birthYear} 
+              onChange={e => setBirthYear(e.target.value)}>
                 <option value='1984'>1984년</option>
                 <option value='1985'>1985년</option>
                 <option value='1986'>1986년</option>
@@ -69,11 +124,19 @@ export default function Form() {
             </select>
 
             <label htmlFor="birthYear">자기소개</label>
-            <textarea value={""} />
+            <textarea value={description} onChange={e=> setDescription(e.target.value)} />
 
             <fieldset>
                 <legend>약관동의</legend>
-                <input id="agree-prov" type="checkbox" name="agreeProv" value= { "yes" } defaultChecked={ false } />
+                <input 
+                  id="agree-prov" 
+                  type="checkbox" 
+                  name="agreeProv" 
+                  value= { agreeProv } 
+                  // defaultChecked={ false } 
+                  checked={agreeProv === 'yes'}
+                  onChange = {onChangeAgreeProv}
+                  />
                 <label>서비스 약관에 동의합니다.</label>
             </fieldset>
 
