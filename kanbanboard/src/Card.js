b238fc84e96aad94c8efeb388360ca8e6b154b86
Bella <shdudtnr3939@naver.com>
2022-03-18 10:04:01 +0900

    22.03.18 kanbanboard hint

--- kanbanboard/src/Card.js
+++ kanbanboard/src/Card.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './assets/css/Card.css';
 import TaskList from './TaskList';
 
 const Card = ({title, description, tasks}) => {
+  const [showDetails, setShowDetails] = useState(false);  // 첫 번째는 안 보이게
   return (
     <div className={styles.Card}>
       <div className={styles.Card__Title}>{title}</div>
