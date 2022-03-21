029aedd502c7f5a7e5d458686e48b068a7c98d5b
Bella <shdudtnr3939@naver.com>
2022-03-21 10:52:36 +0900

    22.03.21 validation exercises

--- 2.Component/ex01/src/04/App.js
+++ 2.Component/ex01/src/04/App.js
@@ -5,7 +5,10 @@ const App = function(){
     
   return (
     <div id="App">
-      <MyComponent />    
+      <MyComponent 
+        // props01={'validation 처리'} 
+        props02={100}
+        props03/> 
     </div>
   )
 }
--- 2.Component/ex01/src/04/MyComponent.js
+++ 2.Component/ex01/src/04/MyComponent.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {Fragment} from 'react';
+import {PropTypes} from 'prop-types';
 
-const MyComponent = () => {
+export default function MyComponent({props01, props02, props03}){
   return (
     <Fragment>
       <h2>Property Validation</h2>
+      <span>props01: {props01 ? props01 : '---- NOT SET ----'}</span>
+      <br />
+      <span>props02: {props02 ? props02 : '---- NOT SET ----'}</span>
+      <br />
+      <span>props03: {props03 ? `${props03}` : '---- NOT SET ----'}</span>  {/* '' + props03 이라고 해도 문자열 변환 된다. */}
       
-    </Fragment>
-    
+    </Fragment>   
   )
 }
 
-export default MyComponent
+MyComponent.propTypes = {
+  // PropTypes Validator(primitive)
+  props01 : PropTypes.string,
+  props02 : PropTypes.number.isRequired,
+  props03 : PropTypes.bool.isRequired
+}
+
+// Default Value
+MyComponent.defaultProps = {
+  props01 : 'Default Value',
+  props02 : 10,
+  props03 : true
+}
