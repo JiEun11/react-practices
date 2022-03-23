e8ab0b66374beb09a00d75b6b43765a941ba7e61
Bella <shdudtnr3939@naver.com>
2022-03-23 16:34:48 +0900

    22.03.23 clock : function component life cycle

--- 2.Component/ex05/src/04/App.js
+++ 2.Component/ex05/src/04/App.js
@@ -3,13 +3,37 @@ import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
+  
+    const getCurrentClockTime = () => {
+      const date = new Date();
+      const hours = date.getHours();
+      
+      return{
+        hours : ('0'+hours).slice(-2),
+        minutes : ('0'+date.getMinutes()).slice(-2),
+        seconds : ('0'+date.getSeconds()).slice(-2)
+      }; 
+    };
 
+    const [state, setState] = useState(getCurrentClockTime());
+    
+    useEffect(()=> {
+      console.log("After Rendering");
+    });
+
+    useEffect(()=>{
+      console.log("After Mount");
+      setInterval(function(){
+        setState(getCurrentClockTime());
+      }, 1000);
+
+    }, []);
 
     return (
             <Clock
                 message={'ex05: useEffect Hook example'}
-                hours={10}
-                minutes={25}
-                seconds={40}/>
+                hours={state.hours}
+                minutes={state.minutes}
+                seconds={state.seconds}/>
     );
 }
