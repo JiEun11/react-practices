3f22fe96f21c2eb7b5ec53d70c102fdd0db53705
Bella <shdudtnr3939@naver.com>
2022-03-23 14:36:51 +0900

    22.03.23 emaillist #1

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './assets/scss/App.scss';
 import RegisterForm from './RegisterForm';
 import SearchBar from './SearchBar';
@@ -8,6 +8,19 @@ import data from './assets/json/data.json';
 const App = () => {
   const [emails, setEmails] = useState(data);
   // const [keyword, setKeyword] = useState("");
+
+  useEffect( async ()=> {
+    const response = await fetch('/api',{
+      method : 'get',
+      headers : {
+        'Content-Type' : 'application/json',
+        'Accept': 'application/json'
+      },
+      body: null
+    });
+
+    console.log(response);
+  }, []); // mount 되었을 때 실행되도록
 
   const notifyKeywordChange = function(keyword){
     console.log(keyword);
