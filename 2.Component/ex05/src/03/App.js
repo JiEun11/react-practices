288ba44dbe5384a43ed3abe9469bf511e8a63e95
Bella <shdudtnr3939@naver.com>
2022-03-23 10:25:52 +0900

    22.03.23 clock : class component life cycle

--- 2.Component/ex05/src/03/App.js
+++ 2.Component/ex05/src/03/App.js
@@ -10,14 +10,14 @@ export default class App extends Component {
 
     getCurrentClockTime(){
       const date = new Date();
-
       const hours = date.getHours();
-      const minutes = date.getMinutes();
-      const seconds = date.getSeconds();
-      const session = hours > 11 ? "pm" : "am";
-
+    
       return{
-
+        hours : ('0'+(hours == 0 ? 12 : (hours > 11 ? hours-12 : hours))).slice(-2),
+        minutes : ('0'+date.getMinutes()).slice(-2),
+        seconds : ('0'+date.getSeconds()).slice(-2),
+        session : hours > 11 ? "pm" : "am",
+        ticks : this.state ? this.state.ticks + 1 : 0
       };
     }
 
@@ -25,12 +25,27 @@ export default class App extends Component {
         return (
             <div className='clock-display'>
                 <h2>ex05 - Component LifeCycle Practice</h2>
+                  {
+                    this.state.ticks % 10 == 0 ? 
+                    null : 
                     <Clock 
-                      hours={10}
-                      minutes={10}
-                      seconds={10}
-                      session={'am'}/>
+                      hours={this.state.hours}
+                      minutes={this.state.minutes}
+                      seconds={this.state.seconds}
+                      session={this.state.session}/> 
+                  }
             </div>
         );
+    }
+
+    componentDidMount(){
+      this.interval = setInterval(function(){
+        this.setState(this.getCurrentClockTime()); //setInterval에서의 this는 이 컴포넌트가 아니므로 주의!!
+      }.bind(this), 1000);
+    }
+
+    componentWillUnmount(){
+      console.log("unmount !!!!! >____<");
+      clearInterval(this.interval);
     }
 }
