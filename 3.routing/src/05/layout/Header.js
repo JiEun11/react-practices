18929fb2f680310c103c8609fb6fe4906b794195
Bella <shdudtnr3939@naver.com>
2022-03-31 11:45:02 +0900

    22.03.31 Styling with Sematic markup

--- 3.routing/src/05/layout/Header.js
+++ 3.routing/src/05/layout/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
 import '../assets/scss/layout/Header.scss';
 
 export default function Header() {
     return (
         <header>
-            <h1>Header</h1>
+            <h1><Link to={'/'}>Mysite</Link></h1>
+            {/* <h1><NavLink to={'/'}>Mysite</NavLink></h1> */}
         </header>
     );
 }
