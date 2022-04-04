e90648ab70a3b9bb3f41ce5df51156741558530b
Bella <shdudtnr3939@naver.com>
2022-04-05 08:16:55 +0900

    22.04.04 ag-grid

--- AgGrid/gigaTables/src/01/index.js
+++
@@ -1,8 +0,0 @@
-//import '.App.css';
-import ReactDOM from 'react-dom';
-import { App } from './App.js';
-
-// document
-//   .getElementById('root')
-//   .appendChild(App());
-ReactDOM.render(App(), document.getElementById('root'));
