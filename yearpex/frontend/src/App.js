dcb549103a03bef9dd3061892a2ccff49aa3fc04
Bella <shdudtnr3939@naver.com>
2022-04-06 09:08:44 +0900

    22.04.06 materials button

--- yearpex/frontend/src/App.js
+++ yearpex/frontend/src/App.js
@@ -1,24 +1,9 @@
-// import './App.css';
-import styles from './static/css/style.css';
+import * as React from 'react';
 
-function App() {
-  return (
-    <div className={styles['ly-gnb-header']}>
-      <div className={styles['user-images']}>
-        <img src="./static/css/images/user-img.png" alt="프로필" title="프로필" />
-      </div>
-      <div className={styles['user-name']}>
-        <div className={styles["name"]}>
-            <a href="#none" className={styles["user-setting-btn"]}>홍길동님</a>
-        </div>
-      </div>
-      <div className={styles["user-group"]}>
-        <span className={styles["group"]}>포스코</span>
-        <span className={styles["group-detail"]}>ITGC실 ITAC그룹 AC섹션</span>
-      </div>
-    </div>
+import Button from '@mui/material/Button';
 
-  );
+export default function App() {
+  return <Button variant="contained">Hello World</Button>;
 }
 
-export default App;
+
